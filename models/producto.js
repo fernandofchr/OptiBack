b492beb3f0c217f1be52d713ff0121fbe6b37787
fernandofchr <fernandofchr@gmail.com>
2023-03-11 00:54:40 -0600

    second commit

---
+++ models/producto.js
@@ -0,0 +1,26 @@
+let mongoose = require("mongoose");
+
+
+
+let ProductoEsquema = new mongoose.Schema({
+ id: {
+   type: Number,
+   required: true,
+ },
+ name: {
+   type: String,
+   required:true,
+ },
+ price: {
+   type: Number,
+   required:true,
+ },
+ description: {
+   type: String,
+  
+ },
+ images:[String],
+});
+
+
+module.exports = mongoose.model("Producto", ProductoEsquema);
---
+++ routes/products.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+let productModel = require('../models/Producto');
+
+
+
+/* Method GET users listing. */
+router.get("/", async function (req, res, next) {
+
+  const resultado = await productModel.find();
+  res.json(resultado);
+});
+
+
+/**
+ * Método post 
+ */
+router.post("/", async function (req, res, next) {
+
+  const product = new productModel({
+      id: req.body.id, //Extra el Id pasado por el body
+      description: req.body.description,
+      name: req.body.name,
+      price: req.body.price,
+      images: req.body.images
+    });
+
+    const result = await product.save(); // Lo guarda en Mongo
+    res.json('Registro Agregado exitosamente');
+});
+
+
+
+/**
+ * Método put
+ */
+router.put("/", async function (req, res, next) {
+  const filter = {id: req.query.id}; //Condición de Query
+  const update = {name: req.query.name}; //Campos a modificar
+
+console.log(filter)
+  const resultado = await productModel.findOneAndUpdate(filter, update, {
+    new:true,
+    upsert: true
+  });
+
+
+  res.json("Se actualiza el producto");
+});
+
+/**
+ * Método delete
+ */
+router.delete("/:id", async function (req, res, next) {
+
+
+  //Buscar un producto por ID y regresa una lista
+const resul = await productModel.find({id: req.params.id}).exec();
+
+
+//Si se encontró lo elimina
+if (resul.length > 0) {
+  await productModel.deleteOne({id: req.params.id});
+  res.json("Eliminando producto");
+} else {
+  res.json({error: "No se encontrol el producto con Id " + req.params.id})
+}
+});
+
+
+module.exports = router;
